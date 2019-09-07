ca536b3367a450fc9fb2bb98d64995bd7a7b284f
franzkraus <franzkraus@franzs-mbp.fritz.box>
2019-09-08 00:00:12 +0200

    add project files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=TestCN0428.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/TestCN0428.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=testcn0428.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testcn0428.tar
